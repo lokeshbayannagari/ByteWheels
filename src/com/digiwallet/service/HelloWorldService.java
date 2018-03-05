fd4a67a2e80ea1812900ec8f965faee845b2cb56
Mukesh <mukesh@globalautomationinc.com>
2018-03-05 13:56:06 +0530

    Initial Commit

---
+++ src/com/digiwallet/service/HelloWorldService.java
@@ -0,0 +1,32 @@
+package com.digiwallet.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloWorldService {
+ 
+	@GET
+	@Path("/{name}")
+	public Response getMsg(@PathParam("name") String name) {
+ 
+		String output = "Welcome   : " + name;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+	
+	
+	@GET
+	@Path("/")
+	public Response getDefault() {
+ 
+		String output = "Welcome   : " + "Test";
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
